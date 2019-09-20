38b0f825e0edadd69e6f2e00299de9654c38c333
cthwaite <thwaite@mac.com>
2019-09-20 21:40:07 +0100

    build: Add setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+# coding: utf-8
+
+from setuptools import setup
+from setuptools_rust import Binding, RustExtension
+
+setup(
+    name="sum_as_string",
+    version="0.0.0",
+    rust_extensions=[RustExtension("sum_as_string._string_sum", binding=Binding.PyO3)],
+    packages=["sum_as_string"],
+    zip_safe=False,
+)
