22becadb941e2f862b3ca75acd78185e7c45e33b
cthwaite <thwaite@mac.com>
2019-09-20 23:39:16 +0100

    build: Bump version number in setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools_rust import Binding, RustExtension
 
 setup(
     name="sum_as_string",
-    version="0.0.2",
+    version="0.0.3",
     rust_extensions=[RustExtension("sum_as_string._string_sum", binding=Binding.PyO3)],
     packages=["sum_as_string"],
     zip_safe=False,
