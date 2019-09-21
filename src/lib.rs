077c0b0de7d733c72429ed01d9b9963ed23dc895
cthwaite <thwaite@mac.com>
2019-09-21 10:43:53 +0100

    docs: Add doc-comment to function

--- src/lib.rs
+++ src/lib.rs
@@ -3,12 +3,13 @@ use pyo3::prelude::*;
 use pyo3::wrap_pyfunction;
 
 #[pyfunction]
+/// Format the sum of two numbers as a string.
 fn sum_as_string(a: usize, b: usize) -> PyResult<String> {
     Ok((a + b).to_string())
 }
 
 #[pymodule]
-fn _string_sum(py: Python, m: &PyModule) -> PyResult<()> {
+fn _string_sum(_py: Python, m: &PyModule) -> PyResult<()> {
     m.add_wrapped(wrap_pyfunction!(sum_as_string))?;
 
     Ok(())
